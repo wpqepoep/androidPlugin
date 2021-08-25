eec2f8cb4df94e347597b0cf4b462c4eb9b6969a
wpqepoep <85506871+wpqepoep@users.noreply.github.com>
2021-08-25 17:27:41 +0800

    2.0

--- plugin1/src/main/java/com/top/plugin1/PluginClass.java
+++ plugin1/src/main/java/com/top/plugin1/PluginClass.java
@@ -1,7 +1,11 @@
 package com.top.plugin1;
 
+import android.content.Context;
+
 public class PluginClass {
+
     public static String getString(int a){
+        
         return "number"+a;
     }
 }
